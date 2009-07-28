8d06e0865cb40a1370e27f9ab88d414d9794de66
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2009-07-28 17:33:14 +0000

    Add condition to viewlet manager

--- src/teamrubber/viewletesi/viewletmanager.py
+++ src/teamrubber/viewletesi/viewletmanager.py
@@ -3,7 +3,17 @@ from plone.app.viewletmanager.manager import OrderedViewletManager
 
 class ESIOrderedViewletManager(OrderedViewletManager):
     template = ViewPageTemplateFile("viewletmanager.pt")
-    
+
+    def render(self):
+        template = ESIOrderedViewletManager.template.__of__(self.context)
+        return template(viewlets=self.viewlets)
+
     def isESI(self, viewlet):
-        return True
+        identifier = viewlet.__class__.__module__ + viewlet.__class__.__name__
+        try:
+            registry = self.context.portal_registry
+        except:
+            return False
+        else:
+            pass # XXX TODO
     
