80296bb8bcb323d642efdb2a991f14aba6ece558
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2009-07-28 16:49:21 +0000

    Add viewlet esi product

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='teamrubber.viewletesi',
+      version=version,
+      description="",
+      long_description=open(os.path.join('src', 'teamrubber', 'viewletesi', 'README.txt')).read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      package_dir = {'':'src'},
+      packages=find_packages('src', exclude=['ez_setup']),
+      namespace_packages=['teamrubber'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
