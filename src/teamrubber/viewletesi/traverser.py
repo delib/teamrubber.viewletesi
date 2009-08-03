78d895830b34bfe7ed03097067be6579d3390dc2
Alan Hoey <alan.hoey@teamrubber.com>
2009-08-03 08:56:30 +0000

    Adding workflow fix to deliverance branch

--- src/teamrubber/viewletesi/traverser.py
+++ src/teamrubber/viewletesi/traverser.py
@@ -6,6 +6,8 @@ from zope.component import getMultiAdapter
 from zope.viewlet.interfaces import IViewletManager
 from zope.viewlet.interfaces import IViewlet
 from utils import FakeView
+from AccessControl.SecurityManagement import newSecurityManager
+from Products.CMFCore.utils import getToolByName
 
 _marker = object()
 
@@ -22,6 +24,11 @@ class ViewletNamespace(object):
         view = FakeView()
         manager = getMultiAdapter((self.context, self.request, view), IViewletManager, name=manager_name)
         viewlet = getMultiAdapter((self.context, self.request, view, manager), IViewlet, name=viewlet_name)
+        super_user = getToolByName(self.context,'portal_url').getWrappedOwner()
+        authenticated_user = self.request.get('AUTHENTICATED_USER')
+        newSecurityManager(self.request,super_user)
         view = self.context.restrictedTraverse('viewlet_renderer')
         view.setViewlet(viewlet)
+        newSecurityManager(self.request,authenticated_user)
         return view
+        
