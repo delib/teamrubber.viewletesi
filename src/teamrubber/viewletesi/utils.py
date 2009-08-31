be2e7ac39232f3f8b68d3d742086d21c9ea955f8
Alan Hoey <alan.hoey@teamrubber.com>
2009-08-31 00:28:08 +0000

    Adding fix to deliverance magic branch

--- src/teamrubber/viewletesi/utils.py
+++ src/teamrubber/viewletesi/utils.py
@@ -1,6 +1,7 @@
 from zope.publisher.interfaces.browser import IBrowserView
+from plone.app.layout.globals.interfaces import IViewView
 from zope.interface import implements
 
 class FakeView(object):
     """ A lightweight fake view for getting viewlets/viewlet managers """
-    implements(IBrowserView)
+    implements(IBrowserView, IViewView)
